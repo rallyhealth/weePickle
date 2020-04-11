b0cb75896aca9ae101e8ff77a32c31e1868501ad
Doug Roper <doug.roper@rallyhealth.com>
2020-04-10 23:36:41 -0400

    Turn on MiMa.

---
+++ weepickle/src/com/rallyhealth/weepickle/v1/LowPriorityImplicits.scala
@@ -0,0 +1,9 @@
+package com.rallyhealth.weepickle.v1
+
+import java.time.ZonedDateTime
+
+abstract class LowPriorityImplicits extends AttributeTagged {
+
+  implicit val FromZonedDateTime: From[ZonedDateTime] = FromString.comap[ZonedDateTime](_.toString)
+  implicit val ToZonedDateTime: To[ZonedDateTime] = new MapStringTo(s => ZonedDateTime.parse(s.toString))
+}
