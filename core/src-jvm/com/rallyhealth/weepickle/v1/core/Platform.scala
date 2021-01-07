2308a80d55cb90cb51b5af3ed0f2d47ccdec8318
Jean Michel Rouly <michel.rouly@gmail.com>
2021-01-07 17:02:52 -0500

    break

--- core/src-jvm/com/rallyhealth/weepickle/v1/core/Platform.scala
+++ core/src-jvm/com/rallyhealth/weepickle/v1/core/Platform.scala
@@ -1,6 +1,7 @@
 package com.rallyhealth.weepickle.v1.core
 
 object Platform {
+  hi
   @inline def charAt(s: CharSequence, i: Int) = s.charAt(i)
   @inline def charAt(s: String, i: Int) = s.charAt(i)
   @inline def byteAt(s: Array[Byte], i: Int) = s(i)
