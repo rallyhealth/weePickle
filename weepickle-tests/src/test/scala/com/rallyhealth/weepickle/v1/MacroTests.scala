d20954ca50318d38ab7d0c2a07943d6782868f3d
Doug Roper <roperdj@gmail.com>
2021-07-14 15:20:53 -0400

    Add @implicitNotFound for From/To/FromTo (#83)

--- weepickle-tests/src/test/scala/com/rallyhealth/weepickle/v1/MacroTests.scala
+++ weepickle-tests/src/test/scala/com/rallyhealth/weepickle/v1/MacroTests.scala
@@ -65,6 +65,7 @@ object Twilight {
 object Pony {
   implicit val pickler = WeePickle.macroFromTo[Pony]
 }
+case class MissingPicklers()
 
 object MacroTests extends TestSuite {
 
@@ -313,6 +314,12 @@ object MacroTests extends TestSuite {
 
     test("map") {
       rw[Pony](Twilight(), """{"$type": "twi"}""")
+    }
+
+    test("compile errors") {
+      compileError("implicitly[WeePickle.To[MissingPicklers]]").msg ==> """Could not find an implicit WeePickle.To[com.rallyhealth.weepickle.v1.MissingPicklers]. Consider adding one with `object com.rallyhealth.weepickle.v1.MissingPicklers { implicit val pickleTo: WeePickle.To[com.rallyhealth.weepickle.v1.MissingPicklers] = macroTo }`"""
+      compileError("implicitly[WeePickle.From[MissingPicklers]]").msg ==> """Could not find an implicit WeePickle.From[com.rallyhealth.weepickle.v1.MissingPicklers]. Consider adding one with `object com.rallyhealth.weepickle.v1.MissingPicklers { implicit val pickleFrom: WeePickle.From[com.rallyhealth.weepickle.v1.MissingPicklers] = macroFrom }`"""
+      compileError("implicitly[WeePickle.FromTo[MissingPicklers]]").msg ==> """Could not find an implicit WeePickle.FromTo[com.rallyhealth.weepickle.v1.MissingPicklers]. Consider adding one with `object com.rallyhealth.weepickle.v1.MissingPicklers { implicit val pickler: WeePickle.FromTo[com.rallyhealth.weepickle.v1.MissingPicklers] = macroFromTo }`"""
     }
   }
 }
