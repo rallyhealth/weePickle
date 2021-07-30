8d9a430e990b2f342f3f680d60156ab3f477e961
Doug Roper <doug.roper@rallyhealth.com>
2021-07-30 15:44:04 -0400

    Add macro tests for changing default params

--- weepickle-tests/src/test/scala/com/rallyhealth/weepickle/v1/DefaultsTests.scala
+++ weepickle-tests/src/test/scala/com/rallyhealth/weepickle/v1/DefaultsTests.scala
@@ -1,10 +1,13 @@
 package com.rallyhealth.weepickle.v1
 
 import com.rallyhealth.weejson.v1.jackson.{FromJson, ToJson}
-import com.rallyhealth.weepickle.v1.WeePickle.{FromScala, ToScala}
+import com.rallyhealth.weejson.v1.{Num, Obj, Value}
+import com.rallyhealth.weepickle.v1.WeePickle.{FromScala, ToScala, ToValue}
 import com.rallyhealth.weepickle.v1.core.FromInput
 import com.rallyhealth.weepickle.v1.implicits.dropDefault
 import utest._
+
+import java.util.concurrent.atomic.AtomicLong
 
 object IntDefaultTests extends TestSuite {
 
@@ -130,5 +133,28 @@ object FromInputStringTests extends TestSuite {
       val fromInput: FromInput = FromScala(A(null))
       FromScala(fromInput).transform(ToJson.string)
     } ==> """{"d":null}""")
+  }
+}
+
+object ChangingDefaultTests extends TestSuite {
+
+  var currentTimeMillis = 0
+  case class Timestamp(@dropDefault ts: Long = currentTimeMillis)
+  implicit val pickler: WeePickle.FromTo[Timestamp] = WeePickle.macroFromTo[Timestamp]
+
+  override val tests: Tests = Tests {
+    test("from") {
+      currentTimeMillis = 0
+      val timestamp1 = FromScala(Timestamp(0)).transform(ToValue) ==> Obj()
+      currentTimeMillis = 1
+      val timestamp2 = FromScala(Timestamp(0)).transform(ToValue) ==> Obj("ts" -> Num(0))
+    }
+
+    test("to") {
+      currentTimeMillis = 2
+      val timestamp1 = FromScala(Obj()).transform(ToScala[Timestamp]).ts ==> 2
+      currentTimeMillis = 3
+      val timestamp2 = FromScala(Obj()).transform(ToScala[Timestamp]).ts ==> 3
+    }
   }
 }
