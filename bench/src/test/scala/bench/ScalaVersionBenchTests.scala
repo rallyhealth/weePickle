c19556fdfd7cc6d7901511a867259ff198cea30b
Doug Roper <doug.roper@rallyhealth.com>
2021-08-09 20:35:18 -0400

    Refactor ScalaVersionBench

--- bench/src/test/scala/bench/ScalaVersionBenchTests.scala
+++ bench/src/test/scala/bench/ScalaVersionBenchTests.scala
@@ -1,26 +1,14 @@
 package bench
 
-import bench.ScalaVersionBench.Data
-import com.rallyhealth.weepickle.v1.WeePickle
-import upickle.core.Visitor
 import utest._
 
 object ScalaVersionBenchTests extends TestSuite {
 
   val tests = Tests {
-    test("fromSample") {
-      new ScalaVersionBench()
-        .testableFromSample(
-          WeePickle.to[Seq[Data]]
-          .map { data =>
-            data ==> ScalaVersionBench.benchmarkSampleData
-            "assertion reached"
-          }
-      ) ==> "assertion reached"
-    }
-
-    test("toSample") {
-      new ScalaVersionBench().toSample ==> ScalaVersionBench.benchmarkSampleData
+    test("SampleBench") {
+      val bench = new SampleBench()
+      test("toWeePickle")(bench.toWeePickle ==> bench.data)
+      test("toUPickle")(bench.toUPickle ==> bench.data)
     }
   }
 }
